315cbecfc33323caaa5d4354a30d2814d84066fa
Katie Pundt <kzpundt@gmail.com>
2021-12-17 15:22:06 -0800

    add New() route to OrdersController

---
+++ VendorTracker/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorTracker.Models;
+
+namespace VendorTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+  }
+}
